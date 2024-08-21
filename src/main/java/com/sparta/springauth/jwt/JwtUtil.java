a080b80425f83eca9d9bc9e67f81eb5bf3b3d073
ilmechaJu <pawor006@naver.com>
2024-08-21 20:05:49 +0900

    Start JWT

---
+++ src/main/java/com/sparta/springauth/jwt/JwtUtil.java
@@ -0,0 +1,13 @@
+package com.sparta.springauth.jwt;
+
+public class JwtUtil {
+    // JWT 생성
+
+    // 생성된 JWT를 Cookie에 저장
+
+    // Cookie에 들어있던 JMT토큰을 Substring
+
+    // JMT 검증
+
+    // JWT에서 사용자 정보 가져오기
+}
